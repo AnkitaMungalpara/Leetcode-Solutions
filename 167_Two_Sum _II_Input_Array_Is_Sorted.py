10a8cdf2eda174881460286bf810af48d3782efe
AnkitaMungalpara <41205043+AnkitaMungalpara@users.noreply.github.com>
2024-07-17 20:01:00 -0500

    LeetcodeSolution #167

---
+++ 167_Two_Sum _II_Input_Array_Is_Sorted.py
@@ -0,0 +1,29 @@
+"""
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. 
+Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+Solution:
+Time Complexity: O(N)
+Space Complexity: O(1)
+
+"""
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        i = 0
+        j = len(numbers) - 1
+
+        while i < j:
+            if numbers[i] + numbers[j] < target:
+                i += 1
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                return (i+1, j+1)
