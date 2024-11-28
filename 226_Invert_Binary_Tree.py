1671bd28e50ba57ef6b716fcb3c4e0f71e92fba8
AnkitaMungalpara <41205043+AnkitaMungalpara@users.noreply.github.com>
2024-11-27 21:08:23 -0500

    LeetcodeSolution #226

---
+++ 226_Invert_Binary_Tree.py
@@ -0,0 +1,32 @@
+"""
+Given the root of a binary tree, invert the tree, and return its root.
+
+Solution:
+Time Complexity: O(N)
+Space Complexity: O(H)
+
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+      
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if not root:
+            return None
+
+        # swap the children
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
+        
