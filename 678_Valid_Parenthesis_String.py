7387a63837cea339aeb8eaaa03e6dd7efafe5f94
AnkitaMungalpara <41205043+AnkitaMungalpara@users.noreply.github.com>
2025-11-21 15:54:45 -0600

    LeetcodeSolution #678

---
+++ 678_Valid_Parenthesis_String.py
@@ -0,0 +1,50 @@
+"""
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "(*)"
+Output: true
+Example 3:
+
+Input: s = "(*))"
+Output: true
+
+Solution:
+Time Complexity: O(N)
+Space Complexity: O(1)
+
+"""
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+
+        leftMin, leftMax = 0 , 0
+
+        for ch in s:
+            if ch == "(":
+                leftMin, leftMax = leftMin + 1, leftMax + 1
+            elif ch == ")":
+                leftMin, leftMax = leftMin - 1, leftMax - 1
+            else:
+                leftMin, leftMax = leftMin - 1, leftMax + 1
+
+            if leftMax < 0:
+                return False
+            if leftMin < 0:
+                leftMin = 0
+
+        return leftMin == 0
+      
